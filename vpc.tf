6c29f6a3a85ce6b7e95eb4e871ac161466be92a5
paragpingale <paragpingale269@gmail.com>
2024-05-15 16:41:31 +0530

    update..

--- vpc.tf
+++ vpc.tf
@@ -1,12 +1,12 @@
 module "vpc" {
-  source = "terraform-aws-module/vpc/aws"
+  source = "terraform-aws-modules/vpc/aws"
 
   name = var.VPC_NAME
   cidr = var.VpcCIDR
 
-  azs = [ var.Zone1, var.Zone2, var.Zone3 ]
-  private_subnet = [ var.PrivSub1CIDR, var.PrivSub2CIDR, var.PrivSub3CIDR ]
-  public_subnet = [ var.PubSub1CIDR, var.PubSub2CIDR, var.PubSub3CIDR ]
+  azs             = [var.Zone1, var.Zone2, var.Zone3]
+  private_subnets = [var.PrivSub1CIDR, var.PrivSub2CIDR, var.PrivSub3CIDR]
+  public_subnets  = [var.PubSub1CIDR, var.PubSub2CIDR, var.PubSub3CIDR]
 
   enable_nat_gateway   = true
   single_nat_gateway   = true
